4d53bfa6f25118593a29623e8a20845925709ff2
malerator <belik7337@gmail.com>
2022-12-12 13:50:00 +0300

    feat:add mask phone

--- script.js
+++ script.js
@@ -1,53 +1,95 @@
-// const popUp = document.createElement("div");
-// popUp.className = "modal";
-// const form = document.querySelector("form");
-// form.className = "form";
-// const btn = document.querySelector(".btn");
-// const closeBtn = document.createElement("button");
-// closeBtn.className = "close";
-// closeBtn.textContent = "Закрыть";
-// const sendBtn = document.querySelector(".send");
+const popUp = document.createElement("div");
+popUp.className = "modal";
+const form = document.querySelector("form");
+form.className = "form";
+const btn = document.querySelector(".btn");
+const closeBtn = document.createElement("button");
+closeBtn.className = "close";
+closeBtn.type = "button";
+closeBtn.textContent = "Закрыть";
+const sendBtn = document.querySelector(".send");
 
-// form.append(closeBtn);
-// popUp.append(form);
-// document.body.append(popUp);
+form.append(closeBtn);
+popUp.append(form);
+document.body.append(popUp);
 
-// btn.addEventListener("click", openModal);
-// closeBtn.addEventListener("click", closeModal);
-// popUp.addEventListener("click", closeModal);
-// form.addEventListener("click", (event) => event.stopPropagation());
+btn.addEventListener("click", openModal);
+closeBtn.addEventListener("click", closeModal);
+popUp.addEventListener("click", closeModal);
+form.addEventListener("click", (event) => event.stopPropagation());
 
-// function openModal() {
-//   popUp.style.display = "flex";
-//   document.body.style.overflow = "hidden";
-// }
+function openModal() {
+  popUp.style.display = "flex";
+  document.body.style.overflow = "hidden";
+}
 
-// function closeModal() {
-//   popUp.style.display = "none";
-//   document.body.style.overflow = "initial";
-// }
+function closeModal() {
+  popUp.style.display = "none";
+  document.body.style.overflow = "initial";
+}
 
-// function handleSubmit(event) {
-//   event.preventDefault();
-//   const data = new FormData(event.target);
-//   const value = Object.fromEntries(data.entries());
-//   console.log(value);
-// }
+const TOKEN = "5856059976:AAHi68Tu9T8jSghs6j6tlfVk1dZWWPw-PGc";
+const CHAT_ID = "-1001695833016";
+const URL = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
 
-// const TOKEN = "5856059976:AAHi68Tu9T8jSghs6j6tlfVk1dZWWPw-PGc";
-// const CHAT_ID = "1194877334";
-// const URL = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
+form.addEventListener("submit", function (el) {
+  el.preventDefault();
 
-// form.addEventListener("submit", function (el) {
-//   e.preventDefault();
+  let message = `<b>Заявка с сайта</b>\n`;
+  message += `<b>Имя </b>${this.fName.value}\n`;
+  message += `<b>Телефон </b>${this.tel.value}`;
+  // const URL = `https://api.telegram.org/bot${TOKEN}/sendMessage?chat_id=${CHAT_ID}&text=${message}`;
 
-//   let message = `<b>Заявка с сайта</b>\n`;
-//   message += `<b>Имя </b>${this.fName.value}\n`;
-//   message += `<b>Телефон </b>${this.tel.value}`;
+  axios.post(URL, {
+    chat_id: CHAT_ID,
+    parse_mode: "html",
+    text: message,
+  });
+});
 
-//   axios.post(URL, {
-//     chat_id: CHAT_ID,
-//     parse_mode: "html",
-//     text: message,
-//   });
-// });
+document.addEventListener("DOMContentLoaded", function () {
+  var input = document.querySelector(".maskphone");
+
+  input.addEventListener("input", mask);
+  input.addEventListener("focus", mask);
+  input.addEventListener("blur", mask);
+
+  /***/
+  function mask(event) {
+    var blank = "+_ (___) ___-__-__";
+
+    var i = 0;
+    var val = this.value.replace(/\D/g, "").replace(/^8/, "7"); // <---
+
+    this.value = blank.replace(/./g, function (char) {
+      if (/[_\d]/.test(char) && i < val.length) return val.charAt(i++);
+
+      return i >= val.length ? "" : char;
+    });
+
+    if (event.type == "blur") {
+      if (this.value.length == 2) this.value = "";
+    } else {
+      setCursorPosition(this, this.value.length);
+    }
+  }
+
+  /***/
+  function setCursorPosition(elem, pos) {
+    elem.focus();
+
+    if (elem.setSelectionRange) {
+      elem.setSelectionRange(pos, pos);
+      return;
+    }
+
+    if (elem.createTextRange) {
+      var range = elem.createTextRange();
+      range.collapse(true);
+      range.moveEnd("character", pos);
+      range.moveStart("character", pos);
+      range.select();
+      return;
+    }
+  }
+});
