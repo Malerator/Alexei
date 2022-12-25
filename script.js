ed77588f1422689a93c38c1e1acf53b7d1e53e78
malerator <belik7337@gmail.com>
2022-12-25 07:26:09 +0300

    aaaaaaaa

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 const popUp = document.createElement("div");
 popUp.className = "modal";
+
 const form = document.querySelector("form");
 form.className = "form";
+
 const btn = document.querySelector(".btn");
+
 const closeBtn = document.createElement("button");
 closeBtn.className = "close";
 closeBtn.type = "button";
+
 const img = document.createElement("img");
 img.src = "./images/close.svg";
 img.width = "15";
 img.height = "15";
+
 const sendBtn = document.querySelector(".send");
 
 closeBtn.append(img);
-
 form.append(closeBtn);
 popUp.append(form);
 document.body.append(popUp);
@@ -41,9 +45,12 @@ form.addEventListener("submit", function (el) {
   el.preventDefault();
 
   let message = `<b>Заявка с сайта</b>\n`;
+  message += `<b>Модель </b>${this.model.value}\n`;
+  message += `<b>Марка </b>${this.mark.value}\n`;
+  message += `<b>Пробег </b>${this.run.value}\n`;
+  message += `<b>Год выпуска </b>${this.year.value}\n`;
   message += `<b>Имя </b>${this.fName.value}\n`;
   message += `<b>Телефон </b>${this.tel.value}`;
-  // const URL = `https://api.telegram.org/bot${TOKEN}/sendMessage?chat_id=${CHAT_ID}&text=${message}`;
 
   axios.post(URL, {
     chat_id: CHAT_ID,
@@ -54,6 +61,8 @@ form.addEventListener("submit", function (el) {
   closeModal();
 });
 
+/***/
+
 document.addEventListener("DOMContentLoaded", function () {
   var input = document.querySelector(".maskphone");
 
@@ -61,12 +70,13 @@ document.addEventListener("DOMContentLoaded", function () {
   input.addEventListener("focus", mask);
   input.addEventListener("blur", mask);
 
-  /***/
+  //   /***/
+
   function mask(event) {
     var blank = "+_ (___) ___-__-__";
 
     var i = 0;
-    var val = this.value.replace(/\D/g, "").replace(/^8/, "7"); // <---
+    var val = this.value.replace(/\D/g, "").replace(/^8/, "7");
 
     this.value = blank.replace(/./g, function (char) {
       if (/[_\d]/.test(char) && i < val.length) return val.charAt(i++);
@@ -81,7 +91,8 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
-  /***/
+  //   /***/
+
   function setCursorPosition(elem, pos) {
     elem.focus();
 
@@ -100,3 +111,84 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 });
+
+// /****/ гребанная форма!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+var currentTab = 0; // Current tab is set to be the first tab (0)
+showTab(currentTab); // Display the crurrent tab
+
+function showTab(n) {
+  // This function will display the specified tab of the form...
+  var x = document.getElementsByClassName("tab");
+  x[n].style.display = "block";
+  //... and fix the Previous/Next buttons:
+  if (n == 0) {
+    document.getElementById("prevBtn").style.display = "none";
+  } else {
+    document.getElementById("prevBtn").style.display = "inline";
+  }
+  if (n == x.length - 1) {
+    document.getElementById("nextBtn").innerHTML = "Submit";
+  } else {
+    document.getElementById("nextBtn").innerHTML = "Next";
+  }
+  //... and run a function that will display the correct step indicator:
+  fixStepIndicator(n);
+}
+
+function nextPrev(n) {
+  // This function will figure out which tab to display
+  var x = document.getElementsByClassName("tab");
+  // Exit the function if any field in the current tab is invalid:
+  if (n == 1 && !validateForm()) return false;
+  // Hide the current tab:
+  x[currentTab].style.display = "none";
+  // Increase or decrease the current tab by 1:
+  currentTab = currentTab + n;
+  // if you have reached the end of the form...
+  if (currentTab >= x.length) {
+    // ... the form gets submitted:
+
+    form.submit();
+
+    return false;
+  }
+  // Otherwise, display the correct tab:
+  showTab(currentTab);
+}
+
+function validateForm() {
+  // This function deals with validation of the form fields
+  var x,
+    y,
+    i,
+    valid = true;
+  x = document.getElementsByClassName("tab");
+  y = x[currentTab].getElementsByTagName("input");
+  // A loop that checks every input field in the current tab:
+  for (i = 0; i < y.length; i++) {
+    // If a field is empty...
+    if (y[i].value == "") {
+      // add an "invalid" class to the field:
+      y[i].className += " invalid";
+      // and set the current valid status to false
+      valid = false;
+    }
+  }
+  // If the valid status is true, mark the step as finished and valid:
+  if (valid) {
+    document.getElementsByClassName("step")[currentTab].className += " finish";
+  }
+  return valid; // return the valid status
+}
+
+function fixStepIndicator(n) {
+  // This function removes the "active" class of all steps...
+  var i,
+    x = document.getElementsByClassName("step");
+  for (i = 0; i < x.length; i++) {
+    x[i].className = x[i].className.replace(" active", "");
+  }
+  //... and adds the "active" class on the current step:
+  x[n].className += " active";
+}
